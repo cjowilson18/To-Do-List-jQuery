b51c590bc1eb21a84dd818a9e1888564fb91fb68
CJ <Cjowilson18@gmail.com>
2023-06-16 11:03:58 -0400

    Actual-Additions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     console.log("LFG");
-    gandDTasks();
+ 
 
 
       var gandDTasks= function () {
@@ -12,7 +12,7 @@ $(document).ready(function() {
         success: function(response, textStatus) {
             $('#toDoItems').empty();
             response.tasks.forEach(function(task) {
-                $('#toDoItems').append("<div> class='row'><button class='delete' item-id='" + task.id + ">All Done</button><p class='col-xs-8'>" + task.content + "</p>"); 
+            $('#toDoItems').append('<div class="row"><p class= "col-xs-8">' + task.content + '</p><button class= "delete" data-id="' + task.id + '"> BYE </button><input type="checkbox" class= " mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
             })
         },
         error: function (request, textStatus, errorMessage) {
@@ -20,7 +20,7 @@ $(document).ready(function() {
         }
         });   
     }   
-    
+       gandDTasks();
     
     var addToList= function () {
         $.ajax({
@@ -50,18 +50,63 @@ $(document).ready(function() {
     });
     
 
-    
-    $.ajax({
+    var allDone= function (id) {
+       $.ajax({
         type: 'DELETE',
-        url: 'https://fewd-todolist-api.onrender.com/tasks/3338?api_key=213',
+        url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=213',
         success: function (response, textStatus) {
             console.log(response);
+            gandDTasks();
         },
         error: function(request, textStatus, errorMessage) {
             console.log(errorMessage);
         }
+    });  
+    }
+
+    $(document).on('click', '.delete', function() {
+        allDone($(this).data('id'));
     });
 
+
+    var doneDone= function(id) {
+        $.ajax({
+            type: 'PUT',
+            url: 'https://fewd-todolist-api.onrender.com/tasks/'+ id+ '/mark_complete?api_key=213',
+            dataType: 'json',
+            success: function( response, textStatus){
+                console.log(response);
+                gandDTasks();
+            },
+            error: function( request, textStatus, errorMessage){
+                console.log(errorMessage);
+            }
+        })
+    }
+
+    $(document).on('change', '.mark-complete', function (){
+        if (this.checked) {
+            doneDone($(this).data('id'));
+        }
+        else{
+            activeTasks($(this).data('id'));
+        }
+    });
+
+    activeTasks= function (id) {
+        $.ajax({
+            type: 'PUT',
+            url: 'https://fewd-todolist-api.onrender.com/tasks/'+ id+ '/mark_active?api_key=213',
+            dataType: 'json',
+            success: function( response, textStatus){
+                console.log(response);
+                gandDTasks();
+            },
+            error: function( request, textStatus, errorMessage){
+                console.log(errorMessage);
+            }
+        })
+    }
     
     });  
 
